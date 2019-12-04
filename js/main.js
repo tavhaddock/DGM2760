815086065cab777722cadb4b376e8d03af570d59
Tavian Haddock <artaviastudios@gmail.com>
2019-12-04 10:05:22 -0700

    Directory added

--- js/main.js
+++ js/main.js
@@ -1,67 +1,188 @@
-document.querySelector('#company').innerText = "Guess My Number"
+document.querySelector('#titleName').innerText = "Fortune Teller"
 
-document.querySelector('#slogan').innerText = "Loops Project"
+document.querySelector('#slogan').innerText = "Switch Statements"
 
-document.querySelector('#instructions').innerText = "In this game, try to guess a number between 1 and 15 in as few tries as possible. \n You will then be awarded a ribbon for your effort. \n If you want to play again, just simply refresh the page."
+document.querySelector()
 
-// Pick a random number between 0 and 16
-
-let correctNumber = Math.floor(Math.random() * 15)
-
-let guessed = false
-let totalGuesses = 0
-let gamerGuess = 0
-correctNumber += 1
-
-console.log(`The correct number is ${correctNumber}`)
-
-function evalGuess(){
-    totalGuesses += 1 // totalGuesses = totalGuesses +1
-    gamerGuess = document.querySelector('#guess').value
-    console.log(totalGuesses, gamerGuess)
-    const feedback = document.querySelector('#feedback')
-
-    if (gamerGuess == correctNumber) {
-        console.log(`gamerGuess is equal to correctNumber`)
-        feedback.innerText = 'You Win!'
-        giveAward()
-        document.getElementById("myBtn").disabled = true;
-    } else if (gamerGuess > correctNumber && gamerGuess < 16) {
-        feedback.innerText = 'Too high! Try again.'
-    } else if (gamerGuess < correctNumber && gamerGuess > 0) {
-        feedback.innerText = 'Too low! Try again.'
-    } else {
-        feedback.innerText = 'Woops! Please choose a number between 1 and 15.'
-        totalGuesses -= 1
-    }
-
-    document.querySelector('#attempts').innerText = totalGuesses
-
+function randomNumber(top) {
+	var x = Math.random();
+	x = x * top;
+	x = (Math.floor(x)) + 1;
+	return x;
 }
 
-function giveAward() {
-    console.log('Congratulations!')
-    let imagePath = '#'
-    switch(totalGuesses) {
-        case 1:
-        case 2:
-        case 3:
-            imagePath = 'images/ribbon1.png'
-        break
-        case 4:
-        case 5:
-        case 6:
-            imagePath = 'images/ribbon2.png '
-        break
-        default:
-            imagePath = 'images/ribbon3.png'
-        break
-    }
+function getMonth(mon) {
+	var msg;
+	switch(mon) {
+		case 1:
+		msg = 'January';
+		break;
+		case 2:
+		msg = 'February';
+		break;
+		case 3:
+		msg = 'March';
+		break;
+		case 4:
+		msg = 'April';
+		break;
+		case 5:
+		msg = 'May';
+		break;
+		case 6:
+		msg = 'June';
+		break;
+		case 7:
+		msg = 'July';
+		break;
+		case 8:
+		msg = 'August';
+		break;
+		case 9:
+		msg = 'September';
+		break;
+		case 10:
+		msg = 'October';
+		break;
+		case 11:
+		msg = 'November';
+		break;
+		case 12:
+		msg = 'December';
+		break;
+		default:
+		msg = 'Your number is out of range.';
+} //end of switch
+	return msg;
+} //end of function
+function getDay(day) {
+	var msg2;
+	switch(day) {
+		case 1:
+		msg2 = '1st';
+		break;
+		case 2:
+		msg2 = '2nd';
+		break;
+		case 3:
+		msg2 = '3rd';
+		break;
+		case 4:
+		msg2 = '4th';
+		break;
+		case 5:
+		msg2 = '5th';
+		break;
+		case 6:
+		msg2 = '6th';
+		break;
+		case 7:
+		msg2 = '7th';
+		break;
+		case 8:
+		msg2 = '8th';
+		break;
+		case 9:
+		msg2 = '9th';
+		break;
+		case 10:
+		msg2 = '10th';
+		break;
+		case 11:
+		msg2 = '11th';
+		break;
+		case 12:
+		msg2 = '12th';
+		break;
+		case 13:
+		msg2 = '13th';
+		break;
+		case 14:
+		msg2 = '14th';
+		break;
+		case 15:
+		msg2 = '15th';
+		break;
+		case 16:
+		msg2 = '16th';
+		break;
+		case 17:
+		msg2 = '17th';
+		break;
+		case 18:
+		msg2 = '18th';
+		break;
+		case 19:
+		msg2 = '19th';
+		break;
+		case 20:
+		msg2 = '20th';
+		break;
+		case 21:
+		msg2 = '21st';
+		break;
+		case 22:
+		msg2 = '22nd';
+		break;
+		case 23:
+		msg2 = '23rd';
+		break;
+		case 24:
+		msg2 = '24th';
+		break;
+		case 25:
+		msg2 = '25th';
+		break;
+		case 26:
+		msg2 = '26th';
+		break;
+		case 27:
+		msg2 = '27th';
+		break;
+		case 28:
+		msg2 = '28th';
+		break;
+		case 29:
+		msg2 = '29th';
+		break;
+		case 30:
+		msg2 = '30th';
+		break;
+		default:
+		msg2 = 'Your number is out of range.';
+} //end of switch
+	return msg2;
+} //end of function
+function getFortune(frtn) {
+	var msg3;
+	switch(frtn) {
+		case 1:
+		msg3 = 'you will have a root canal.';
+		break;
+		case 2:
+		msg3 = 'you will find $1,000.';
+		break;
+		case 3:
+		msg3 = 'a piano will fall on your head.';
+		break;
+		case 4:
+		msg3 = 'you will win a new car.';
+		break;
+		case 5:
+		msg3 = 'a dog will bite you.';
+		break;
+		default:
+		msg3 = 'Your number is out of range.';
+} //end of switch
+	return msg3;
+} //end of function
 
-    const awardImage = document.createElement('img') // Creates a <img> element
-    awardImage.setAttribute('src', imagePath)
+var monthName = randomNumber(12);
+var month = getMonth(monthName);
+var dayNumber = randomNumber(30);
+var day = getDay(dayNumber);
+var fortuneName = randomNumber(5);
+var fortune = getFortune(fortuneName);
 
-    const ribbon = document.querySelector('#ribbon')
-
-    ribbon.appendChild(awardImage)
-}
+var feedbackMessage = 'On '+month+' '+day+', '+fortune;
+document.getElementById("fortune").innerHTML = feedbackMessage;
